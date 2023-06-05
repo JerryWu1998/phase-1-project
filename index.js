c181745ce3df8a08159812eaf8d624327e9b54bb
Yu Wu <yw3748@columbia.edu>
2023-06-05 13:08:54 -0400

    update menu and big-picture

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
-// Make fetch request to API
-fetch('http://localhost:3000/memes')
-    .then(response => response(json))
-    .then(function(data){
-        accessMemes(data)
-        renderFirstMeme(data[0])
-    });
+// make fetch request to API, use GET to receive all done memes
+fetch("http://localhost:3000/done-memes")
+  .then(response => response.json())
+  .then(data => {
+    // call function to access array
+    accessMemes(data);
+    // set default to show first meme
+    showDetail(data[0]);
+  });
 
-// Function to access array
-function accessMemes(memeArray){
-    // Iterate through array
-    memeArray.forEach(renderMeme)
+
+// function to access array
+function accessMemes(memeArray) {
+  // select the container where to put memes
+  const memeContainer = document.querySelector('#meme-menu');
+  // iterate through array
+  memeArray.forEach((singleMeme) => {
+    // create single meme div
+    const singleMemeContainer = document.createElement('div');
+    const singleMemeImage = document.createElement('img');
+    singleMemeImage.src = singleMeme.image;
+    // append single meme to DOM
+    singleMemeContainer.append(singleMemeImage);
+    memeContainer.append(singleMemeContainer);
+    // add event listener to each meme (click to show name and big picture)
+    singleMemeImage.addEventListener('click', () => {
+      showDetail(singleMeme);
+    })
+  });
+}
+
+
+// after click image, shows detail
+function showDetail(singleMemeData) {
+  // update name and image in detail area
+  const nameDetail = document.querySelector('#detail-name');
+  nameDetail.textContent = singleMemeData.name;
+  const imageDetail = document.querySelector('#detail-image');
+  imageDetail.src = singleMemeData.image;
 }
